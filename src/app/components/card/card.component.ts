03a8c0a826d1a96c40e24612ead5a2b89e2f3c96
Menno Guldemond <mennoguldemond@live.nl>
2025-02-26 09:53:28 +0100

    add card effects to card template

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,10 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { Card } from '@app/models';
 
 @Component({
   selector: 'app-card',
-  imports: [MatCardModule],
+  imports: [CommonModule, MatCardModule, MatTooltipModule],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss',
 })
