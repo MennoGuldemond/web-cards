2e98f100a0586e4900c7927edb4d39ed9aa386d1
Menno Guldemond <mennoguldemond@live.nl>
2025-02-27 11:24:48 +0100

    routerlink to overview

--- src/app/components/menu-bar/menu-bar.component.ts
+++ src/app/components/menu-bar/menu-bar.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-menu-bar',
-  imports: [MatButtonModule, MatIconModule, MatMenuModule],
+  imports: [MatButtonModule, MatIconModule, MatMenuModule, RouterModule],
   templateUrl: './menu-bar.component.html',
   styleUrl: './menu-bar.component.scss',
 })
