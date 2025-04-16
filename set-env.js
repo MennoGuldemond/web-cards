eb9adda13076768504a7648233277d6ddd6b2556
Menno Guldemond <mennoguldemond@live.nl>
2025-04-16 19:43:18 +0200

    remove key from source and regenerate

--- set-env.js
+++ set-env.js
@@ -1,3 +1,11 @@
+const fs = require("fs");
+const dotenv = require("dotenv");
+
+// Load environment variables from .env file
+dotenv.config();
+
+const targetPath = "./src/environments/environment.ts";
+const envConfigFile = `
 import packageJson from '../../package.json';
 
 export const environment = {
@@ -5,7 +13,7 @@ export const environment = {
     projectId: 'web-cards-db8d6',
     appId: '1:304612749434:web:d7a6eaae6fe7ad69a48bc3',
     storageBucket: 'web-cards-db8d6.firebasestorage.app',
-    apiKey: '',
+    apiKey: '${process.env.GOOGLE_API_KEY}',
     authDomain: 'web-cards-db8d6.firebaseapp.com',
     messagingSenderId: '304612749434',
     measurementId: 'G-EBFWN5R40C',
@@ -13,3 +21,8 @@ export const environment = {
   production: false,
   version: packageJson.version,
 };
+`;
+
+// Write the environment file
+fs.writeFileSync(targetPath, envConfigFile);
+console.log(`Environment file generated at ${targetPath}`);
